7d2bd0a7491a2696657ea91f68ce4cbc3a94d6d3
Scott Tolinski <scott.tolinski@teamdetroit.com>
2014-07-22 22:38:48 -0400

    adds angular

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -47,3 +47,143 @@
     }
   });
 })();
+
+
+
+function ColorsController($scope) {
+    $scope.contacts = [
+    [{
+      color: "#e51c23",
+      name: "red",
+      shade: "500"
+    },{
+      color: "#ff5177",
+      name: "red",
+      shade: "A200"
+    }], [{
+      color: "#e91e63",
+      name: "pink",
+      shade: "500"
+    },{
+      color: "#ff4081",
+      name: "pink",
+      shade: "A200"
+    }], [{
+      color: "#9c27b0",
+      name: "purple",
+      shade: "500"
+    },{
+      color: "#e040fb",
+      name: "purple",
+      shade: "A200"
+    }], [{
+      color: "#673ab7",
+      name: "deep purple",
+      shade: "500"
+    },{
+      color: "#7c4dff",
+      name: "deep purple",
+      shade: "A200"
+    }], [{
+      color: "#3f51b5",
+      name: "indigo",
+      shade: "500"
+    },{
+      color: "#536dfe",
+      name: "indigo",
+      shade: "A200"
+    }], [{
+      color: "#5677fc",
+      name: "blue",
+      shade: "500"
+    },{
+      color: "#6889ff",
+      name: "blue",
+      shade: "A200"
+    }], [{
+      color: "#03a9f4",
+      name: "light blue",
+      shade: "500"
+    },{
+      color: "#40c4ff",
+      name: "light blue",
+      shade: "A200"
+    }], [{
+      color: "#00bcd4",
+      name: "cyan",
+      shade: "500"
+    },{
+      color: "#18ffff",
+      name: "cyan",
+      shade: "A200"
+    }], [{
+      color: "#009688",
+      name: "teal",
+      shade: "500"
+    },{
+      color: "#64ffda",
+      name: "teal",
+      shade: "A200"
+    }], [{
+      color: "#259b24",
+      name: "green",
+      shade: "500"
+    },{
+      color: "#5af158",
+      name: "green",
+      shade: "A200"
+    }], [{
+      color: "#8bc34a",
+      name: "light green",
+      shade: "500"
+    },{
+      color: "#b2ff59",
+      name: "light green",
+      shade: "A200"
+    }], [{
+      color: "#cddc39",
+      name: "lime",
+      shade: "500"
+    },{
+      color: "#eeff41",
+      name: "lime",
+      shade: "A200"
+    }], [{
+      color: "#ffeb3b",
+      name: "yellow",
+      shade: "500"
+    },{
+      color: "#ffff00",
+      name: "yellow",
+      shade: "A200"
+    }], [{
+      color: "#ffc107",
+      name: "amber",
+      shade: "500"
+    },{
+      color: "#ffd740",
+      name: "amber",
+      shade: "A200"
+    }], [{
+      color: "#ff9800",
+      name: "orange",
+      shade: "500"
+    },{
+      color: "#ffab40",
+      name: "orange",
+      shade: "A200"
+    }]
+
+    ];
+
+    $scope.current = "#333";
+
+    $scope.add = function() {
+      $scope.contacts.push($scope.newcontact);
+      $scope.newcontact = "";
+    }
+
+    $scope.getSelectedColor = function(color) {
+      $scope.current = color;
+    }
+}
