1c447e802590e5e71c84487d5b5942c438aa5384
Scott Tolinski <scott.tolinski@teamdetroit.com>
2014-07-26 15:09:58 -0400

    adds contrasting text

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -52,144 +52,193 @@
 
 
 
-var launch = angular.module('launch', ['ngClipboard']);
+var launch = angular.module('launch', ['ngClipboard', 'ngRoute']);
+
+launch.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/about', {
+        templateUrl: 'partials/about.html'
+      }).
+      when('/creator-info', {
+        templateUrl: 'partials/creator-info.html'
+      }).
+      otherwise({
+        templateUrl: 'partials/colors.html'
+      });
+  }]);
+
 
 launch.controller('ColorsController', function($scope) {
     $scope.contacts = [
     [{
       color: '#e51c23',
       name: 'red',
-      shade: '500'
+      shade: '500',
+      text: '#FFF'
     },{
       color: '#ff5177',
       name: 'red',
-      shade: 'A200'
+      shade: 'A200',
+      text: '#FFF'
     }], [{
       color: '#e91e63',
       name: 'pink',
-      shade: '500'
+      shade: '500',
+      text: '#FFF'
     },{
       color: '#ff4081',
       name: 'pink',
-      shade: 'A200'
+      shade: 'A200',
+      text: '#FFF'
     }], [{
       color: '#9c27b0',
       name: 'purple',
-      shade: '500'
+      shade: '500',
+      text: '#FFF'
     },{
       color: '#e040fb',
       name: 'purple',
-      shade: 'A200'
+      shade: 'A200',
+      text: '#FFF'
     }], [{
       color: '#673ab7',
       name: 'deep purple',
-      shade: '500'
+      shade: '500',
+      text: '#FFF'
     },{
       color: '#7c4dff',
       name: 'deep purple',
-      shade: 'A200'
+      shade: 'A200',
+      text: '#FFF'
     }], [{
       color: '#3f51b5',
       name: 'indigo',
-      shade: '500'
+      shade: '500',
+      text: '#FFF'
     },{
       color: '#536dfe',
       name: 'indigo',
-      shade: 'A200'
+      shade: 'A200',
+      text: '#FFF'
     }], [{
       color: '#5677fc',
       name: 'blue',
-      shade: '500'
+      shade: '500',
+      text: '#FFF'
     },{
       color: '#6889ff',
       name: 'blue',
-      shade: 'A200'
+      shade: 'A200',
+      text: '#FFF'
     }], [{
       color: '#03a9f4',
       name: 'light blue',
-      shade: '500'
+      shade: '500',
+      text: '#FFF'
     },{
       color: '#40c4ff',
       name: 'light blue',
-      shade: 'A200'
+      shade: 'A200',
+      text: '#FFF'
     }], [{
       color: '#00bcd4',
       name: 'cyan',
-      shade: '500'
+      shade: '500',
+      text: '#FFF'
     },{
       color: '#18ffff',
       name: 'cyan',
-      shade: 'A200'
+      shade: 'A200',
+      text: '#212121'
     }], [{
       color: '#009688',
       name: 'teal',
-      shade: '500'
+      shade: '500',
+      text: '#FFF'
     },{
       color: '#64ffda',
       name: 'teal',
-      shade: 'A200'
+      shade: 'A200',
+      text: '#212121'
     }], [{
       color: '#259b24',
       name: 'green',
-      shade: '500'
+      shade: '500',
+      text: '#FFF'
     },{
       color: '#5af158',
       name: 'green',
-      shade: 'A200'
+      shade: 'A200',
+      text: '#212121'
     }], [{
       color: '#8bc34a',
       name: 'light green',
-      shade: '500'
+      shade: '500',
+      text: '#FFF'
     },{
       color: '#b2ff59',
       name: 'light green',
-      shade: 'A200'
+      shade: 'A200',
+      text: '#212121'
     }], [{
       color: '#cddc39',
       name: 'lime',
-      shade: '500'
+      shade: '500',
+      text: '#212121'
     },{
       color: '#eeff41',
       name: 'lime',
-      shade: 'A200'
+      shade: 'A200',
+      text: '#212121'
     }], [{
       color: '#ffeb3b',
       name: 'yellow',
-      shade: '500'
+      shade: '500',
+      text: '#212121'
     },{
       color: '#ffff00',
       name: 'yellow',
-      shade: 'A200'
+      shade: 'A200',
+      text: '#212121'
     }], [{
       color: '#ffc107',
       name: 'amber',
-      shade: '500'
+      shade: '500',
+      text: '#212121'
     },{
       color: '#ffd740',
       name: 'amber',
-      shade: 'A200'
+      shade: 'A200',
+      text: '#212121'
     }], [{
       color: '#ff9800',
       name: 'orange',
-      shade: '500'
+      shade: '500',
+      text: '#FFF'
     },{
       color: '#ffab40',
       name: 'orange',
-      shade: 'A200'
+      shade: 'A200',
+      text: '#FFF'
     }], [{
       color: '#ff5722',
       name: 'deep orange',
-      shade: '500'
+      shade: '500',
+      text: '#FFF'
     },{
       color: '#ff6e40',
       name: 'deep orange',
-      shade: 'A200'
+      shade: 'A200',
+      text: '#FFF'
     }]
 
     ];
 
     $scope.current = '#3F51B5';
     $scope.accent = '#ff4081';
+    $scope.text = '#FFF';
+
 
     $scope.add = function() {
       $scope.contacts.push($scope.newcontact);
@@ -197,13 +246,15 @@ launch.controller('ColorsController', function($scope) {
     }
 
     $scope.getSelectedColor = function(color) {
+      console.log($scope);
       if (color.shade == '500') {
         $scope.current = color.color;
+        $scope.text = color.text;
       } else if (color.shade == 'A200') {
         $scope.accent = color.color;
       }
-
     }
+
 
     $scope.getTextToCopy = function(color) {
         return color.color;
